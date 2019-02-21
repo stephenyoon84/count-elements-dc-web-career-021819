7ced08fbd4fa674c582bf6f2f1da54b3dd244e26
stephenyoon84 <stephenyoon84@gmail.com>
2019-02-20 21:17:40 -0500

    add solution

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
   # code goes here
+  result = {}
+  array.each do |x|
+    if result[x]
+      result[x] += 1
+    else
+      result[x] = 1
+    end
+  end
+  result
 end
- 
